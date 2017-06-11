5f0d514f916daa2a14245a63fa742db34935bcdf
Brett East <brett@sproutback.com>
2017-06-11 11:58:18 +1000

    Add tests for Signup

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Accounts } from 'meteor/accounts-base';
 import { Link } from 'react-router';
+import { createContainer } from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
 
-export default class Signup extends React.Component {
+export class Signup extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -21,7 +23,7 @@ export default class Signup extends React.Component {
       });
     }
 
-    Accounts.createUser({email, password}, (err) => {
+    this.props.createUser({email, password}, (err) => {
       if (err) {
         this.setState({
           error: err.reason
@@ -53,3 +55,13 @@ export default class Signup extends React.Component {
     );
   }
 };
+
+Signup.propTypes = {
+  createUser: PropTypes.func.isRequired
+};
+
+export default createContainer(() => {
+  return {
+    createUser: Accounts.createUser
+  };
+}, Signup);
